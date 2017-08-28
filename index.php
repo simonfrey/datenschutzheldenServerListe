07995670946e692a906c77cfd11783d5bfbcd1db
Simon Frey <s.frey@campus.tu-berlin.de>
2017-08-29 01:14:33 +0200

    Init

---
+++ index.php
@@ -0,0 +1,362 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+<meta charset="utf-8" />
+<title>XMPP/Jabber Server Liste | Datenschutzhelden.org</title>
+
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="css/bootstrap-theme.min.css">
+<link rel="stylesheet" type="text/css" href="css/footable.bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+<?php
+ include ("hitcounter.php");
+?>
+	<h1>XMPP/Jabber Server Liste <sub>handverlesen mit <span class="glyphicon glyphicon-heart"></span> </sub></h1>
+	<h4>ein <a href="https://datenschutzhelden.org/2017/07/12/daten-sparsame-xmpp-server/" target="_blank">datenschutzhelden.org</a> Projekt</h4>
+	<hr>
+<?php
+ include("datatable.html");
+?>
+<table class="table" data-sorting="true">
+	<thead>
+		<tr>
+			<th>Domäne</th>
+			<th data-breakpoints="xs sm">Kein IP-Logging</th>
+			<!--<th data-breakpoints="xs sm">Kein Logging von Nutzeraktivität</th>-->
+			<th data-breakpoints="xs">Minimalistische Registrierung</th>
+			<th data-breakpoints="xs sm">Gehashtes PW</th>
+			<th data-breakpoints="xs">Enforced Encryption</th>
+			<th data-breakpoints="xs">Hidden Service</th>
+			<th data-breakpoints="xs sm md">SOCKS5 Bytestreams</th>
+			<th data-breakpoints="xs">Push Notifications</th>
+			<th data-breakpoints="xs sm">XMPP Over TLS</th>
+			<th data-breakpoints="xs sm md">HTTP-Upload Limit / Speicherdauer</th>
+			<th data-breakpoints="xs sm md">Kontakt-Möglichkeit</th>
+		</tr>
+	</thead>
+	<tbody>
+
+
+		<tr>
+			<td><a href="https://systemli.org" target="_blank">systemli.org</a></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<!--<td><span class="glyphicon glyphicon-thumbs-up"></span></td>-->
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-down"></span></td>
+			<td>1 MB / 30 Tage</td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+		</tr>
+
+		<tr>
+			<td><a href="http://jabber.hot-chilli.net" target="_blank">jabber.hot-chilli.net</a></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<!--<td><span class="glyphicon glyphicon-thumbs-up"></span></td>-->
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td>10 MB / 31 Tage</td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+		</tr>
+
+		<tr>
+			<td><a href="http://simplewire.de" target="_blank">simplewire.de</a></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<!--<td><span class="glyphicon glyphicon-thumbs-up"></span></td>-->
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-down"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td>15 MB / 14 Tage</td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+		</tr>
+
+		<tr>
+			<td><a href="http://retrotalk.de" target="_blank">retrotalk.de</a></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<!--<td><span class="glyphicon glyphicon-thumbs-up"></span></td>-->
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-down"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-down"></span></td>
+			<td>4 MB / 14 Tage</td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+		</tr>	
+		<tr>
+			<td><a href="http://magicbroccoli.de" target="_blank">magicbroccoli.de</a></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<!--<td><span class="glyphicon glyphicon-thumbs-up"></span></td>-->
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-down"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td>10 MB / 31 Tage</td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+		</tr>
+
+
+		<tr>
+			<td><a href="http://bommboo.de" target="_blank">bommboo.de</a></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<!--<td><span class="glyphicon glyphicon-thumbs-up"></span></td>-->
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td>10 MB / 30 Tage</td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+		</tr>
+
+
+
+
+		<tr>
+			<td><a href="http://jabjab.de" target="_blank">jabjab.de</a></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<!--<td><span class="glyphicon glyphicon-thumbs-up"></span></td>-->
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td>50 MB / 30 Tage</td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+		</tr>
+
+		<tr>
+			<td><a href="http://trashserver.net" target="_blank">trashserver.net</a></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<!--<td><span class="glyphicon glyphicon-thumbs-up"></span></td>-->
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td>50 MB / 30 Tage</td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+		</tr>
+
+
+		<tr>
+			<td><a href="http://wiuwiu.de" target="_blank">wiuwiu.de</a></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<!--<td><span class="glyphicon glyphicon-thumbs-up"></span></td>-->
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td>10 MB / 30 Tage</td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+		</tr>
+
+
+		<tr>
+			<td><a href="http://pimux.de" target="_blank">pimux.de</a></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<!--<td><span class="glyphicon glyphicon-thumbs-up"></span></td>-->
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td>10 MB / 30 Tage</td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+		</tr>
+
+
+		<tr>
+			<td><a href="http://jabber.cat" target="_blank">jabber.cat</a></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<!--<td><span class="glyphicon glyphicon-thumbs-up"></span></td>-->
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td>1 MB / 7 Tage</td>
+			<td><span class="glyphicon glyphicon-thumbs-down"></span></td>
+		</tr>
+
+
+
+		<tr>
+			<td><a href="http://draugr.de" target="_blank">draugr.de</a></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<!--<td><span class="glyphicon glyphicon-thumbs-up"></span></td>-->
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-down"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td>15 MB / 30 Tage</td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+		</tr>
+
+		<tr>
+			<td><a href="http://riotcat.org" target="_blank">riotcat.org</a></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<!--<td><span class="glyphicon glyphicon-thumbs-up"></span></td>-->
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-down"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td> 10 MB / 14 Tage</td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+		</tr>
+
+		<tr>
+			<td><a href="http://elaon.de" target="_blank">elaon.de</a></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<!--<td><span class="glyphicon glyphicon-thumbs-up"></span></td>-->
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-down"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-down"></span></td>
+			<td>10 MB / 30 Tage</td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+		</tr>
+
+
+		<tr>
+			<td><a href="http://jabber-germany.de" target="_blank">jabber-germany.de</a></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<!--<td><span class="glyphicon glyphicon-thumbs-up"></span></td>-->
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td>10 MB / 7 Tage</td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+		</tr>
+
+		<tr>
+			<td><a href="http://jabber.de" target="_blank">jabber.de</a></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<!--<td><span class="glyphicon glyphicon-thumbs-up"></span></td>-->
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-down"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-down"></span></td>
+			<td>10 MB / 7 Tage</td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+		</tr>
+
+		<tr>
+			<td><a href="http://5222.de" target="_blank">5222.de</a></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<!--<td><span class="glyphicon glyphicon-thumbs-down"></span></td>-->
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td>50 MB / 30 Tage</td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+		</tr>
+
+		<tr>
+			<td><a href="http://dismail.de" target="_blank">dismail.de</a></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span><sub>[1]</sub></td>
+			<!--<td><span class="glyphicon glyphicon-thumbs-up"></span></td>-->
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td>25 MB / 31 Tage</td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+		</tr>
+
+		<tr>
+			<td><a href="http://jabber.at" target="_blank">jabber.at</a></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span><sub>[2]</sub></td>
+			<!--<td><span class="glyphicon glyphicon-thumbs-down"></span></td>-->
+			<td><span class="glyphicon glyphicon-thumbs-down"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-down"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-down"></span></td>
+			<td><span class="glyphicon glyphicon-thumbs-down"></span></td>
+			<td>30 MB / 30 Tage</td>
+			<td><span class="glyphicon glyphicon-thumbs-up"></span></td>
+		</tr>
+	</tbody>
+</table>
+<div>
+<b>Footnotes</b><br>
+[1] Bei Registrierung wird die IP-Adresse zur Spam-Erkennung für 3 Tage (Out-Band) respektive 21 Tage (In-Band) gespeichert.<br>
+[2] Bei der Out-Band Registrierung wird die IP-Adresse zur Spam-Erkennung für 1 Monat gespeichert.
+</div>
+<br>
+<div>
+Weitere Informationen zu dieser Liste findet ihr in unserem zugehörigen Artikel auf <a href="https://datenschutzhelden.org/2017/07/12/daten-sparsame-xmpp-server/" target="_blank">datenschutzhelden.org</a>
+</div>
+<div>
+Betreibt ihr euren eigenen Server, oder kennt ihr einen Server den wir noch auf die Liste setzen sollen? Kontaktiert uns: <a href="mailto:thorsten@datenschutzhelden.org">thorsten@datenschutzhelden.org</a></div>
+<div align="right">
+	Letzte Aktualisierung: 17.08.2017
+</div>
+<script src="js/jquery-3.2.1.min.js"></script> 
+<script src="js/bootstrap.min.js"></script> 
+<script src="js/footable.min.js"></script> 
+
+<script>
+jQuery(function($){
+	$('.table').footable();
+});
+</script>
+
+</body>
+</html>
